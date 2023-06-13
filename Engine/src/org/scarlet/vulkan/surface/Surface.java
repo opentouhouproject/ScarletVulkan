5519102d52b625d16e7abb4f3fa05c5585e825b8
Open-Touhou <opentouhouproject@gmail.com>
2023-06-13 15:34:11 -0400

    Initial implementation of the Surface class.
    - Initial implementation of the Surface class.

---
+++ Engine/src/org/scarlet/vulkan/surface/Surface.java
@@ -0,0 +1,60 @@
+package org.scarlet.vulkan.surface;
+
+import org.lwjgl.glfw.GLFWVulkan;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.KHRSurface;
+import org.lwjgl.vulkan.VkInstance;
+import org.scarlet.Engine;
+import org.scarlet.EngineLogger;
+import org.scarlet.vulkan.device.PhysicalDevice;
+
+import java.nio.LongBuffer;
+import java.util.logging.Level;
+
+/**
+ * Representation of a surface to display rendering results on.
+ */
+public class Surface {
+    /**
+     * Physical hardware device.
+     */
+    private final PhysicalDevice physicalDevice;
+
+    /**
+     * The handle to the Vulkan surface.
+     */
+    private long surface;
+
+    /**
+     * Constructor.
+     * @param physicalDevice The physical hardware device.
+     * @param windowHandle The handle of the window to create the surface for.
+     */
+    public Surface(PhysicalDevice physicalDevice, long windowHandle) {
+        EngineLogger.getInstance().log(Level.INFO, "Creating Vulkan surface.");
+        this.physicalDevice =physicalDevice;
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            LongBuffer pSurface = stack.mallocLong(1);
+            VkInstance vulkanInstance = this.physicalDevice.getDevice().getInstance();
+            GLFWVulkan.glfwCreateWindowSurface(vulkanInstance, windowHandle, null, pSurface);
+            surface = pSurface.get(0);
+        }
+    }
+
+    /**
+     * Free resources.
+     */
+    public void cleanup() {
+        EngineLogger.getInstance().log(Level.INFO, "Destroying Vulkan surface.");
+        VkInstance vulkanInstance = physicalDevice.getDevice().getInstance();
+        KHRSurface.vkDestroySurfaceKHR(vulkanInstance, surface, null);
+    }
+
+    /**
+     * Retrieve the handle to the Vulkan surface.
+     * @return long - The handle to the surface.
+     */
+    public long getSurface() {
+        return surface;
+    }
+}
